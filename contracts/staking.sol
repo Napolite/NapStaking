416b464c5283730800c320eb7464ceb8fc5500cc
Constantino <Okonemmanuelmma@gmail.com>
2023-05-18 23:38:18 +0100

    #feature : deposit function

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.1 ;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 interface IStake{
     function depositStake( uint amount) external;
@@ -17,11 +18,13 @@ interface IStake{
 }
 
 contract Staking{
+    using SafeMath for uint256;
+    using SafeMath for uint;
     IERC20 public stakingToken;
     IERC20 public rewardsToken;
 
-    uint public totalStakeSupply;
-    uint public totalRewardsGenerated;
+    uint public totalStake;
+    uint public totalRewardsPaid;
     
     uint public duration; // stake duration
     uint public rate; //stake rate
@@ -35,4 +38,14 @@ contract Staking{
         stakingToken = IERC20(_stakeToken);
         rewardsToken = IERC20(_rewardsToken);
     }
+
+    function depositStake (uint amount) public {
+        require(stakingToken.balanceOf(msg.sender) >= amount, "Not enough tokens for stake");
+        require(stakingToken.transferFrom(msg.sender, address(this), amount),"Failed to stake token");
+
+        stakeBalance[msg.sender] = stakeBalance[msg.sender].add(amount);
+        totalStake = totalStake.add(amount);
+    }
+
+    
 }
